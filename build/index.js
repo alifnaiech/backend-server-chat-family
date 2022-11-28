69e3135b83505f412b093ac4181b6e62720c4945
alifnaiech <alifnaiech@gmail.com>
2022-11-28 23:06:07 +0100

    created connection with socket.io

--- build/index.js
+++ build/index.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log('working on chat app with socket io');
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+app.get("/user", (req, res, next) => {
+    res.json('users');
+});
+app.listen(3000, () => console.log('server is listening'));
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
-console.log('working on chat app with socket io');
+import express from "express";
+import { createServer } from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const httpServer  = createServer(app);
+const io = new Server(httpServer, {
+    cors: {
+        origin: '*'
+    }
+});
+io.on("connection", socket => {
+    console.log('user is connected !')
+    socket.on('message', (message)=>{
+        console.log(`Message from ${socket.id} :  ${message}`);
+        io.emit('message', `${socket.id.substring(0,2)} said ${message}`)
+    });
+
+    socket.on('disconnect', ()=>{
+        console.log('user disconnected');
+    })
+})
+
+httpServer.listen(3000, ()=> console.log('server is runung'));
